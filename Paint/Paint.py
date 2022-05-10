c6c893796fb5cc01294a93373a405ef6e0363cdd
Kevin-alberto01 <84153631+Kevin-alberto01@users.noreply.github.com>
2022-05-10 11:14:48 -0500

    Update Paint.py

--- Paint/Paint.py
+++ Paint/Paint.py
@@ -1,3 +1,5 @@
+from math import*
+
 from turtle import *
 
 from freegames import vector
@@ -25,19 +27,45 @@ def square(start, end):
     end_fill()
 
 
-def circle(start, end):
+def circle2(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x,start.y)
+    down()
+    begin_fill()
+    d= math.sqrt(((end.x - start.x)**2)+((end.y - start.y)**2))
+    circle(d/2)
+    end_fill()
 
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 
 def tap(x, y):
@@ -58,10 +86,33 @@ def store(key, value):
     state[key] = value
 
 
-state = {'start': None, 'shape': line}
-setup(420, 420, 370, 0)
+def info_alumnos():
+    up()
+    goto(14,190)
+    color('blue')
+    write("Kevin ALberto Crisostomo A00832188", align='right',font=('Arial',10,'normal'))
+    goto(18,170)
+    color('pink')
+    write("Alejandro Melendez Torres A00832494", align='right',font=('Arial',10,'normal'))
+    goto(35,150)
+    color('green')
+    write("José Edmundo Romo Castillo A01197772", align='right',font=('Arial',10,'normal'))
+    
+
+
+
+state = {'start': None, 'shape': line }
+setup(420, 420, 500, 0)
+info_alumnos() 
+#Programacion basada en eventos 
+#funcion que atinde los eventos del mosuse
+
+
 onscreenclick(tap)
 listen()
+
+
+#funcion que atiene los eventos del teclado
 onkey(undo, 'u')
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
@@ -70,7 +121,8 @@ onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', circle2), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
+#siempre debe ser la ultima instrucción- glutmainloop()- 
 done()
